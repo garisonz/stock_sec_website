05c3dc36dafabdb511fc2f756ca332d962b6f975
Garison Zagorski <garisonzag88@gmail.com>
2024-01-10 01:01:55 -0500

    changes to sec.py

--- stockApp/sec.py
+++ stockApp/sec.py
@@ -1,9 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import urllib.request
+import lxml
+import unicodedata
+import re
 
-# https://data.sec.gov/submissions/CIK{cik}.json
 # json parse 
+
+def restore_windows_1252_characters(restore_string):
+    """
+        Replace C1 control characters in the Unicode string s by the
+        characters at the corresponding code points in Windows-1252,
+        where possible.
+    """
+
+    def to_windows_1252(match):
+        try:
+            return bytes([ord(match.group(0))]).decode('windows-1252')
+        except UnicodeDecodeError:
+            # No character at the corresponding code point: remove it.
+            return ''
+        
+    return re.sub(r'[\u0080-\u0099]', to_windows_1252, restore_string)
 
 def fetch_and_parse_json(url, headers):
     try:
@@ -17,7 +36,8 @@ def fetch_and_parse_json(url, headers):
         print(f"An error occurred: {err}")
 
 # URL pointing to the SEC JSON data
-url = 'https://data.sec.gov/submissions/CIK0000320193.json'
+# https://data.sec.gov/api/xbrl/companyconcept/CIK0000{cik}/us-gaap/AccountsPayableCurrent.json
+url = 'https://data.sec.gov/api/xbrl/companyconcept/CIK0000789019/us-gaap/AccountsPayableCurrent.json'
 
 # Headers including a User-Agent
 headers = {
@@ -32,9 +52,75 @@ if json_data:
     # The structure of JSON needs to be known to navigate it properly
     # Here's an example of how you might access the primaryDocument
     # Adjust the key access based on the actual structure of your JSON data
-    recent_filings = json_data.get('filings', {}).get('recent', {})
-    if 'primaryDocument' in recent_filings:
-        primary_document = recent_filings['primaryDocument']
-        print("Primary Document:", primary_document)
-    else:
-        print("Primary document not found in the JSON data.")
+    list = []
+    list = json_data['units'].get('USD')
+
+    last_item = len(list)
+    i = len(list)-1
+
+    # prints all 10k
+
+    #filings = []
+
+    #while i > 0:
+    #    if list[0:last_item][i].get('form') == '10-K':
+    #        print(list[0:last_item][i])
+    #        filings.append(list[0:last_item][i])
+    #        i -= 1
+    #        continue
+        
+    #    i -= 1
+
+    # prints the most recent 10k & 10q
+
+    while i > 0:
+        if list[0:last_item][i].get('form') == '10-K':
+            print(list[0:last_item][i])
+            break
+        
+        i -= 1
+    
+    while i > 0:
+        if list[0:last_item][i].get('form') == '10-Q':
+            print(list[0:last_item][i])
+            break
+        
+        i -= 1
+
+# 'https://www.sec.gov/Archives/edgar/data/{cik}/{accn no -}/{accn}.txt'
+        
+weburl = 'https://www.sec.gov/Archives/edgar/data/789019/000095017023035122/0000950170-23-035122.txt'
+
+page = requests.get(weburl, headers=headers)
+
+print("HTTP Status Code:", page.status_code)
+
+# Check if the request was successful
+if page.status_code == 200:
+
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    v = soup.find('sec-header')
+    print(v.get_text())
+
+    for tag in soup.find_all(True):
+        print(tag.name)
+
+    z = soup.find('document')
+    content = z.get_text()
+    content = content.strip()
+    content = ''.join(content.split())
+    file_path = 'stocks\stockApp\output.txt'
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+    
+
+    
+    
+    
+
+else:
+    print("Failed to fetch the web page")
